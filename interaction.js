e4c5856e72ddee5ca7c27435e90d4224c6428f56
Shaly S <148628634+shalysurjen@users.noreply.github.com>
2024-11-09 23:16:05 +0530

    Add files via upload

---
+++ interaction.js
@@ -0,0 +1,11 @@
+// Function to show an alert when a product is added to the cart
+function addToCart() {
+    alert("Product has been added to your cart!");
+}
+
+// Function to navigate to product details
+function viewProductDetails() {
+    window.location.href = "product-details.html";
+}
+
+// Optional: You can add more interactivity functions as needed.
